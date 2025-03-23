d22aadb4278e690073eaf146012f19df3b730d2d
Ruben <rubenernestovalle@gmail.com>
2025-03-22 21:29:00 -0600

    ACT Construction first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,ts}"], // Asegúrate de que incluya archivos .astro
+  theme: {
+    fontFamily:{
+      sans: ['Gotham SSm A, sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+};
